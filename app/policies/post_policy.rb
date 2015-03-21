4e73c2e99c3e9b1e36c33e5395f7aa1443fad1d6
Foster Kizer <kizer713@gmail.com>
2015-03-21 12:20:39 -0700

    Complete Authorization assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,24 @@
 class PostPolicy < ApplicationPolicy
 	
+	class Scope
+		attr_reader :user, :scope
+		
+		def initialize(user, scope)
+			@user = user
+			@scope = scope
+		end
+		
+		def resolve
+			if user.present? && user.admin?
+				scope.all
+			elsif user.present?
+				user.posts
+			else
+				scope.none
+			end
+		end
+	end
+	
 	def index?
 		true
 	end
