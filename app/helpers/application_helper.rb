f0b93117e02103f32ad180c16f6669c5a6caf4d9
Foster Kizer <kizer713@gmail.com>
2015-04-01 23:06:20 -0700

    Fixed typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
 	def markdown_to_html(markdown)
 		renderer = Redcarpet::Render::HTML.new
 		extensions = {fenced_code_blocks: true}
-		recarpet = Recarpet::Markdown.new(renderer, extensions)
-		(recarpet.render markdown).html_safe
+		redcarpet = Recarpet::Markdown.new(renderer, extensions)
+		(redcarpet.render markdown).html_safe
 	end
 end
