6a8a8f5111baaf870c6ecf86ddc58c92bb284c1c
Foster Kizer <kizer713@gmail.com>
2015-04-08 21:53:39 -0700

    Destroy checkpoint completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
   
 	def create
 		@topic = Topic.find(params[:topic_id])
-		@post = Post.find(params[:post_id])
+		@post = @topic.posts.find(params[:post_id])
 		@comment = current_user.comments.build(comment_params)
 		@comment.post = @post
 		authorize @comment
@@ -22,6 +22,19 @@ class CommentsController < ApplicationController
 		end
 	end
 	
+	def destroy
+		@topic = Topic.find(params[:topic_id])
+		@post = @topic.posts.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		authorize @comment 
+		if @comment.destroy
+			flash[:notice] = "Comment was removed."
+			redirect_to [@topic, @post]
+		else
+			flash[:error] = "Comment couldn't be deleted. Try again."
+			redirect_to [@topic, @post]
+		end
+	end
 	
 	private
 	
