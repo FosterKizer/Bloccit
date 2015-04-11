164f0cdbbc1217afe43e4c8c69f703e6096bad1b
Foster Kizer <kizer713@gmail.com>
2015-04-11 15:50:36 -0700

    Completed Mocking RSpec checkpoint

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,6 +41,7 @@ require 'faker'
    
    # sets created_at time within the past year
    post.update_attributes!(created_at: rand(10.minutes .. 1.year).ago)
+   post.create_vote
    post.update_rank
  end
  posts = Post.all
