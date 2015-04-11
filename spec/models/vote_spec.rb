84e579edcf0d7db1cb4a4ae8f3d63cd3da1697fb
Foster Kizer <kizer713@gmail.com>
2015-04-11 15:06:36 -0700

    Fixed vote_spec validation test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -3,8 +3,29 @@ require 'rails_helper'
 describe Vote do
 	describe "validations" do
 		describe "value validation" do
-			it "only allows -1 or 1 as values" do
-				expect( invalid_vote.valid? ).to eq(false)
+			describe "when greater than 1" do
+				subject { Vote.new(value: 2) }
+				it { should_not be_valid }
+			end
+			
+			describe "when equal to 1" do
+				subject { Vote.new(value: 1) }
+				it { should be_valid }
+			end
+			
+			describe "when equal to 0" do
+				subject { Vote.new(value: 0) }
+				it { should_not be_valid }
+			end
+			
+			describe "when equal to -1" do
+				subject { Vote.new(value: -1) }
+				it { should be_valid }
+			end
+			
+			describe "when less than -1" do
+				subject { Vote.new(value: -2) }
+				it { should_not be_valid }
 			end
 		end
 	end
